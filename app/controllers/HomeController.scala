adc6b0f0667625918a5e1c701b04e0d0aef9c3e5
ISTANBULBEKLE <ekipkalir@gmail.com>
2021-05-17 14:09:25 +0100

    HomeControllerSpec is updated

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,14 +1,12 @@
 package controllers
 
 import models.Vehicle
-
 import javax.inject._
 import play.api._
 import play.api.libs.json.Json
 import play.api.mvc._
 import play.api.libs.ws._
 import play.mvc.Results.status
-
 import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.ExecutionContext.global
 
@@ -33,26 +31,15 @@ class HomeController @Inject()(ws: WSClient, val controllerComponents: Controlle
 
   def vehicle(vehicleName:String): Action[AnyContent]= Action.async{implicit request:Request[AnyContent] =>
       val futureResult = ws.url(s"http://localhost:9001/vehicle/${vehicleName}").get()
-        println("This is response " + futureResult)
+        // println("This is response " + futureResult)
+
     futureResult.map { response =>
-      //println(response.json.getClass)
       val js = Json.fromJson[Vehicle](response.json)
       val veh = js.get
       Ok(views.html.vehicle(veh))
-      //      Ok(response.json)
+    } recover {
+      case _ => NotFound
     }
   }
 
 }
-
-//val futureResult: Future[String] = ws.url(url).get().map { response =>
-//  (response.json \ "person" \ "name").as[String]
-//  }
-
-//def getById(itemId: Long) = Action {
-//  val foundItem = todoList.find(_.id == itemId)
-//  foundItem match {
-//  case Some(item) => Ok(Json.toJson(item))
-//  case None => NotFound
-//  }
-//  }
